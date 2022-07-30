744d148015dcbc19812cb9d5f2729cd492c6803f
shmato4ek <58270694+shmato4ek@users.noreply.github.com>
2022-07-30 11:49:10 +0300

    Added mail sender

--- src/sender/sender.go
+++ src/sender/sender.go
@@ -1,11 +1,72 @@
 package sender
 
-import "btc_service/src/model"
+import (
+	"btc_service/src/btc"
+	"btc_service/src/model"
+	"btc_service/src/persistance"
+	"crypto/tls"
+	"strconv"
+
+	"gopkg.in/gomail.v2"
+)
 
 type (
-	sender   struct{}
+	sender struct {
+	}
 	database interface {
 		Save()
-		Read() model.Email
+		Exists(model.Email) bool
 	}
 )
+
+type MailSender struct {
+	From      string
+	To        []model.Email
+	Subject   string
+	Text      string
+	Attach    string
+	password  string
+	smtp_name string
+}
+
+func New(from string, smtpName string, password string, text string, subject string) *MailSender {
+	var mailSender MailSender
+	mailSender.From = from
+	mailSender.smtp_name = smtpName
+	mailSender.password = password
+	mailSender.Text = text
+	mailSender.Subject = subject
+	return &mailSender
+}
+
+func (mailSender *MailSender) send() {
+	m := gomail.NewMessage()
+
+	m.SetHeader("From", mailSender.From)
+
+	addresses := make([]string, len(mailSender.To))
+	for i, recipient := range mailSender.To {
+		addresses[i] = m.FormatAddress(string(recipient), "")
+	}
+
+	m.SetHeader("To", addresses...)
+
+	m.SetHeader("Subject", mailSender.Subject)
+
+	m.SetBody("text/plain", mailSender.Text)
+
+	d := gomail.NewDialer(mailSender.smtp_name, 465, mailSender.From, mailSender.password)
+
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
+}
+
+func (mailSender *MailSender) SendRate(fdb *persistance.FileDatabase) {
+	btc_rate := btc.GetRate()
+	mailSender.To = fdb.Buffer
+	mailSender.Text += strconv.Itoa(btc_rate)
+	mailSender.send()
+}
