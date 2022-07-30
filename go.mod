3c495eddce9abf6d158282bd990eaaae669f2fa4
shmato4ek <58270694+shmato4ek@users.noreply.github.com>
2022-07-30 11:50:20 +0300

    Added .env file

--- go.mod
+++ go.mod
@@ -2,7 +2,12 @@ module btc_service
 
 go 1.18
 
+require github.com/CoinAPI/coinapi-sdk v0.0.0-20220701043655-c1a87ff7aa73
+
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0
+	github.com/shopspring/decimal v1.3.1 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,65 @@
 package main
 
 import (
+	"btc_service/src/btc"
+	"btc_service/src/model"
 	"btc_service/src/persistance"
+	"btc_service/src/sender"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
+func goDotEnvVariable(key string) string {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+const (
+	filepath = "C:/Users/gangt/OneDrive/Documents/GitHub/btc_service"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func getRate(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(btc.GetRate())
+}
+
+func subscribe(w http.ResponseWriter, r *http.Request) {
+	var email string
+	err := json.NewDecoder(r.Body).Decode(&email)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fdb := persistance.New(filepath, goDotEnvVariable("DATABASE_FILE_NAME"))
+	fdb.Save(model.Email(email))
+}
+
+func sendEmails(w http.ResponseWriter, r *http.Request) {
+	fdb := persistance.New(filepath, goDotEnvVariable("DATABASE_FILE_NAME"))
+	sender := sender.New(goDotEnvVariable("EMAIL_FOR_SENDING"), goDotEnvVariable("SMTP_NAME"), goDotEnvVariable("EMAIL_PASSWORD"), goDotEnvVariable("MAIL_TEXT"), goDotEnvVariable("MAIL_SUBJECT"))
+	sender.SendRate(fdb)
+}
+
 func main() {
-	database := persistance.New("D:/BTC service/src")
-	fmt.Println(database.Exists("m.shmatko8@gmail.com"))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/rate", getRate).Methods("GET")
+	router.HandleFunc("/subscribe", subscribe).Methods("POST")
+	router.HandleFunc("/sendEmails", sendEmails).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
